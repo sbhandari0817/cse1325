0efee95034f02991762bde88b19a0cc6ad9c7af6
santosh <santosh.bhandari2@mavs.uta.edu>
2020-01-22 14:27:45 -0600

    Full Credit Assignment 1

---
+++ P01/full_credit/hello.cpp
@@ -0,0 +1,10 @@
+/*This is first program in c++ to print hello world. 
+Author: Santosh Bhandari
+ID:10001387116
+*/
+#include <iostream>
+using namespace std; //Optional
+int main()
+{ 
+	cout<< "Hello Santosh" <<endl;
+}
