a3431e66c64e15ff479ed132773cdeff2607aa7b
santosh <santosh.bhandari2@mavs.uta.edu>
2020-01-22 14:54:27 -0600

    bonus points assignment 1

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,13 @@
+/*This is the bonus question for assigment one in which program will ask user to enter the name  to print 
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	//Asking user to enter the name
+	string name;
+	cout <<"Please enter your name: "<<endl;
+	cin >>name;
+	cout <<"Hello "<<name<<endl;
+}
+
