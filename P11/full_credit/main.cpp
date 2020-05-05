3a5e0fdda7bec0f12810638b82810419682298dd
sbhandri0817 <santosh.bhandari2@mavs.uta.edu>
2020-05-05 00:09:41 -0500

    main

--- P11/full_credit/main.cpp
+++ P11/full_credit/main.cpp
@@ -1,4 +1,11 @@
 #include "cart.h"
+#include "item.h"
 int main(){
-
+	std::string name;
+	double price;
+	Cart kroger{"Santosh"};
+	Item item{name, price };
+	std::cout<<"Enter product names and price (e.g., "English peas 0.79"):"<<std::endl
+	std::cin >>item;
+	std::cout<<kroger;
 }
